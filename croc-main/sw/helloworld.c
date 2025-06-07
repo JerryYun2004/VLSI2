07eecc032cb90c19747680eb956446f07ec3fb81
Simon Zhu <simonzhu132435@gmail.com>
2025-06-07 17:21:30 +0200

    Add working user ROM to print names

--- croc-main/sw/helloworld.c
+++ croc-main/sw/helloworld.c
@@ -4,7 +4,7 @@
 //
 // Authors:
 // - Philippe Sauter <phsauter@iis.ee.ethz.ch>
-
+#include "config.h"
 #include "uart.h"
 #include "print.h"
 #include "timer.h"
@@ -13,56 +13,72 @@
 
 /// @brief Example integer square root
 /// @return integer square root of n
-uint32_t isqrt(uint32_t n) {
-    uint32_t res = 0;
-    uint32_t bit = (uint32_t)1 << 30;
+// uint32_t isqrt(uint32_t n) {
+//     uint32_t res = 0;
+//     uint32_t bit = (uint32_t)1 << 30;
 
-    while (bit > n) bit >>= 2;
+//     while (bit > n) bit >>= 2;
 
-    while (bit) {
-        if (n >= res + bit) {
-            n -= res + bit;
-            res = (res >> 1) + bit;
-        } else {
-            res >>= 1;
-        }
-        bit >>= 2;
-    }
-    return res;
-}
+//     while (bit) {
+//         if (n >= res + bit) {
+//             n -= res + bit;
+//             res = (res >> 1) + bit;
+//         } else {
+//             res >>= 1;
+//         }
+//         bit >>= 2;
+//     }
+//     return res;
+// }
 
-int main() {
+// int main() {
+//     uart_init(); // setup the uart peripheral
+
+//     // simple printf support (only prints text and hex numbers)
+//     printf("Hello World!\n");
+//     // wait until uart has finished sending
+//     uart_write_flush();
+
+//     // toggling some GPIOs
+//     gpio_set_direction(0xFFFF, 0x000F); // lowest four as outputs
+//     gpio_write(0x0A);  // ready output pattern
+//     gpio_enable(0xFF); // enable lowest eight
+//     // wait a few cycles to give GPIO signal time to propagate
+//     asm volatile ("nop; nop; nop; nop; nop;");
+//     printf("GPIO (expect 0xA0): 0x%x\n", gpio_read());
+
+//     gpio_toggle(0x0F); // toggle lower 8 GPIOs
+//     asm volatile ("nop; nop; nop; nop; nop;");
+//     printf("GPIO (expect 0x50): 0x%x\n", gpio_read());
+//     uart_write_flush();
+
+//     // doing some compute
+//     uint32_t start = get_mcycle();
+//     uint32_t res   = isqrt(1234567890UL);
+//     uint32_t end   = get_mcycle();
+//     printf("Result: 0x%x, Cycles: 0x%x\n", res, end - start);
+//     uart_write_flush();
+
+//     // using the timer
+//     printf("Tick\n");
+//     sleep_ms(10);
+//     printf("Tock\n");
+//     uart_write_flush();
+//     return 1;
+// }
+
+
+int main() { 
+
     uart_init(); // setup the uart peripheral
-
-    // simple printf support (only prints text and hex numbers)
-    printf("Hello World!\n");
-    // wait until uart has finished sending
+    printf("The content of the ROM is:\n");
+    for (int i = 0; i < 32 ; i++) {
+    char c = *reg8(USER_ROM_BASE_ADDR, i);
+    if (c == '\0') break;
+    printf("%c", c);
+}
+    printf("\n");
     uart_write_flush();
 
-    // toggling some GPIOs
-    gpio_set_direction(0xFFFF, 0x000F); // lowest four as outputs
-    gpio_write(0x0A);  // ready output pattern
-    gpio_enable(0xFF); // enable lowest eight
-    // wait a few cycles to give GPIO signal time to propagate
-    asm volatile ("nop; nop; nop; nop; nop;");
-    printf("GPIO (expect 0xA0): 0x%x\n", gpio_read());
-
-    gpio_toggle(0x0F); // toggle lower 8 GPIOs
-    asm volatile ("nop; nop; nop; nop; nop;");
-    printf("GPIO (expect 0x50): 0x%x\n", gpio_read());
-    uart_write_flush();
-
-    // doing some compute
-    uint32_t start = get_mcycle();
-    uint32_t res   = isqrt(1234567890UL);
-    uint32_t end   = get_mcycle();
-    printf("Result: 0x%x, Cycles: 0x%x\n", res, end - start);
-    uart_write_flush();
-
-    // using the timer
-    printf("Tick\n");
-    sleep_ms(10);
-    printf("Tock\n");
-    uart_write_flush();
     return 1;
 }
