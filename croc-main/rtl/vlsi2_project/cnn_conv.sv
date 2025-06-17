1e20b09fe6ead5be32a6c332c2236fe81c7dfa5e
JerryYun2004 <160551336+JerryYun2004@users.noreply.github.com>
2025-06-18 01:32:57 +0800

    Simulation cnn_conv.sv
    Updated for simulation

--- croc-main/rtl/vlsi2_project/cnn_conv.sv
+++ croc-main/rtl/vlsi2_project/cnn_conv.sv
@@ -1,17 +1,21 @@
-// Convolution module: 3x3 kernel
-module conv #(parameter DATA_WIDTH = 8, ACC_WIDTH = 32) (
-    input logic signed [DATA_WIDTH-1:0] window[0:8],
-    input logic signed [DATA_WIDTH-1:0] weight[0:8],
-    output logic signed [ACC_WIDTH-1:0] conv_out
+// Updated Convolution Module
+module conv #(
+    parameter DATA_WIDTH = 8,
+    parameter ACC_WIDTH  = 32
+) (
+    input  logic signed [DATA_WIDTH-1:0] window[0:8],
+    input  logic signed [DATA_WIDTH-1:0] weight[0:8],
+    output logic signed [ACC_WIDTH-1:0]  conv_out
 );
 
+    logic signed [ACC_WIDTH-1:0] sum;
+
     always_comb begin
-        conv_out = '0;
-    
-        (* unroll *)  // Hint for the synthesizer to unroll this loop
+        sum = 0;
         for (int i = 0; i < 9; i++) begin
-            conv_out += window[i] * weight[i];
+            sum += window[i] * weight[i];
         end
+        conv_out = sum;
     end
 
 endmodule
