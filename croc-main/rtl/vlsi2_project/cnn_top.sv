bd00fde857f596ad30893040251dd83e99f06bd2
JerryYun2004 <160551336+JerryYun2004@users.noreply.github.com>
2025-07-11 19:52:17 +0800

    fml stupidest mistake ever

--- croc-main/rtl/vlsi2_project/cnn_top.sv
+++ croc-main/rtl/vlsi2_project/cnn_top.sv
@@ -33,10 +33,6 @@ module cnn_top #(
     output logic                  user_mem_write_en
 );
 
-
-    typedef obi_req_t #(ObiCfg) obi_req_t_;
-    typedef obi_rsp_t #(ObiCfg) obi_rsp_t_;
-    
     // Memory-mapped default patch for Croc compatibility
     localparam logic [ADDR_WIDTH-1:0] DEFAULT_INPUT_BASE  = 32'h1A10_0000;
     localparam logic [ADDR_WIDTH-1:0] DEFAULT_OUTPUT_BASE = 32'h1A10_0010;
